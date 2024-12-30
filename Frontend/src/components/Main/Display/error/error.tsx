17966b35706f91ae292b43eb0d36f37ef4f32a7b
Apoorva Jha <apoorvaj20@iitk.ac.in>
2024-12-30 22:40:17 +0530

    Initialized Frontend Project

--- Frontend/src/components/Main/Display/error/error.tsx
+++
@@ -1,5 +0,0 @@
-function Error<T>(props: T) {
-  if (props.err !== null) return <div>Error hai</div>;
-  else return <div>Nahi hai error</div>;
-}
-export default Error;
--- Frontend/src/components/Main/Display/success/success.tsx
+++
@@ -1,4 +0,0 @@
-function Result<T>(props: T) {
-  return <>LOL</>;
-}
-export default Result;
